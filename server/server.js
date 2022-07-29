8a67db7c8acc984612c706ec23510f9906f27b2a
Andrew <armcburn@gmail.com>
2022-07-29 12:32:32 -0400

    Refactor serverjs code for deployment on heroku

--- server/server.js
+++ server/server.js
@@ -9,14 +9,19 @@ import smsRequestRoute from "./routes/smsRequestRoutes.js";
 import usersRoute from "./routes/userRoutes.js";
 import messagesRoute from "./routes/messageRoutes.js";
 
+const port = process.env.PORT || 5000;
+
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
 const app = express();
 dotenv.config();
 app.use(cors());
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-
-const port = process.env.PORT || 5000;
 
 app.use("/api/sms-requests", smsRequestRoute);
 app.use("/api/users", usersRoute);
@@ -36,14 +41,4 @@ if (process.env.NODE_ENV === "production") {
   app.get("/", (req, res) => res.send("Set to production."));
 }
 
-mongoose
-  .connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() =>
-    app.listen(port, () => {
-      console.log(`Server running on ${port}.`);
-    })
-  )
-  .catch((err) => console.log(err));
+app.listen(port, () => console.log(`Server started on port ${port}`));
